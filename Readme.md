e9555b75b43bfd516be25a98260ef6e58def5801
hassaan_aslam <hassaanaslam0321@gmail.com>
2025-12-30 18:11:23 +0500

    next js app

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Next JS
+In this repository I will push code of my Next Js learning
+I will mention my progress here whenever i contribute to this repository
+
+## Day 1
+
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-4xl mt-4 font-mono text-center text-[#c6f12d]'>Hello Hassaan!</h1>
+      <p className='mt-8 ml-4'>So today we are using Next JS </p>
+    </div>
+  )
+}
+export default page
