91dab236a232de42aa49400e721abea27a22ae6c
hassaan_aslam <hassaanaslam0321@gmail.com>
2025-12-31 23:04:47 +0500

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
 const page = () => {
   return (
     <div>
       <h1 className='text-4xl mt-4 font-mono text-center text-[#c6f12d]'>Hello Hassaan!</h1>
-      <p className='mt-8 ml-4'>So today we are using Next JS </p>
+      <div className='mt-8 ml-4'>
+      <p>So today we are using Next JS </p>
+      <h1 className='text-2xl text-[#61d5e7]'>Now we are creating Routes to check how it actually works.</h1>
+      <h1 className='text-2xl text-[crimson]'>Now I am creating a folder of about.</h1>
+      </div>
     </div>
   )
 }
