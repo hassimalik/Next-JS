0612cecdad768088100d612ccebafc111db69f6f
hassaan_aslam <hassaanaslam0321@gmail.com>
2026-01-13 16:43:41 +0500

    update Header.tsx, page.tsx and page.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header(){
         <header className='p-4 mb-6 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 rounded-lg shadow-lg'>
                 <h1 className='text-3xl font-bold mb-2 text-white'>Teams Section</h1>
                 <nav className='flex space-x-4 '>
-                    <Link className=' p-1 rounded-xl  transition-all  transform hover:scale-120 hover:bg-[#672208] ' href="/teams">Home</Link>{" "}
+                    <Link className=' p-1 rounded-xl  transition-all  transform hover:scale-120 hover:bg-[#672208] ' href="/">Home</Link>{" "}
                     <Link className=' p-1 rounded-xl  transition-all  transform hover:scale-120 hover:bg-[#220651]' href="/teams/Quetta">Quetta</Link>{" "}
                     <Link className=' p-1 rounded-xl  transition-all  transform hover:scale-120 hover:bg-[#ead811]' href="/teams/Peshawar">Peshawar</Link>{" "}
                     <Link className=' p-1 rounded-xl  transition-all  transform hover:scale-120 hover:bg-[#074243]' href="/teams/Multan">Multan</Link>{" "}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,30 @@ const page = () => {
   return (
     <div>
       <h1 className='text-4xl mt-4 font-mono text-center text-[#c6f12d]'>Hello Hassaan!</h1>
-      <div className='mt-8 ml-4'>
+      <div className='mt-8 ml-4 flex gap-4 flex-col'>
       <p>So today we are using Next JS </p>
       <h1 className='text-2xl text-[#61d5e7]'>Now we are creating Routes to check how it actually works.</h1>
       <h1 className='text-2xl text-[crimson]'>Now I am creating a folders to create routes.</h1>
       So today i will discuss about the hooks of next js
       These are :
-      <ol className='text-red-400'>
+      <ol className= 'text-red-400 flex flex-col gap-4'>
         <li>useParams</li>
+        <p className='text-white'>Use params is a hook which is used to view params like in which id in which params we are present  that are dynamic routes</p>
         <li>useRouter</li>
+        <p className='text-white'>Use Router is a hook that is used to give dynamic routes</p>
         <li>useSearchParams</li>
         <li>usePathname</li>
+        <p className='text-white'>Use Path name is a hook that is used to find and use the exact path at which it is present.</p>
         <li>useEffect</li>
+        <p className='text-white'>Use effect is a hook that it is of both react and next. Use effect is a hook that is used to render apis or something that we want to do one time before our page loads so that we can get the results already before the landing page.</p>
         <li>useState</li>
+        <p className='text-white'>Use state is a hook that is used to save a state or to change a state. For example the most common example is that of counter a button to increase the counting value . It is because of the useState.</p>
+        <li>useContext</li>
+        <p className='text-white'>Use context is a hook that is used to use context of a custom/context api.</p>
       </ol>
       <br />
       <hr />
       <br />
-      <div className='mt-4'>
-        <h1 className='text-center text-2xl text-[#a39104]'>CSR vs SSR</h1>
-        <h2>CSR means Client Side Rendering.</h2>
-        <h2>Client Side means that it is on browser side only or it is </h2>
-      </div>
       </div>
     </div>
   )
